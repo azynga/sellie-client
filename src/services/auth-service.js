815cd18d44abf87af888184f0f19103f041cbc3f
Aljoscha Zynga <aljoscha.zynga@gmail.com>
2022-05-20 13:36:08 +0200

    change client services to use URL from .env file

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 
 const service = axios.create({
-    baseURL: 'http://localhost:5005/api/auth',
+    baseURL: process.env.REACT_APP_API_URL, //'http://localhost:5005/api',
     withCredentials: true,
 });
 
 const signup = (username, password) => {
-    return service.post('/signup', { username, password });
+    return service.post('/auth/signup', { username, password });
 };
 
 const login = (username, password) => {
-    return service.post('/login', { username, password });
+    return service.post('/auth/login', { username, password });
 };
 
 const loggedin = () => {
-    return service.get('/loggedin');
+    return service.get('/auth/loggedin');
 };
 
 const logout = () => {
-    return service.get('/logout');
+    return service.get('/auth/logout');
 };
 
 export { signup, login, loggedin, logout };
